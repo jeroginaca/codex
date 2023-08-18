b2a2efc4f9c540b0b7d8e3ee0105774d1cbbcb55
JeronimoGinaca <jeronimo.ginaca@globant.com>
2023-08-18 15:27:56 +0200

    bugfix

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -2,9 +2,7 @@ import ProfileHeader from "@/components/shared/ProfileHeader";
 import { fetchUser } from "@/lib/actions/user.actions";
 import { currentUser } from "@clerk/nextjs"
 import { redirect } from "next/navigation"
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
-import { profileTabs } from "@/constants";
-import Image from "next/image";
+
 import MyTweets from "@/components/shared/MyTweets";
 
 const Page = async ({ params }: { params: { id: string } }) => {
